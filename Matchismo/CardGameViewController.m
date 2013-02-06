6591dfb88e7a0066dc42ff4e05bf2250b1f40429
Jeroen Wesbeek <work@osx.eu>
2013-02-06 13:42:07 +0100

    - adding CardMatchingGame model - put the business logic in the CardMatchingGame model - changed the controller to use the model

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -8,32 +8,45 @@
 
 #import "CardGameViewController.h"
 #import "PlayingCardDeck.h"
+#import "CardMatchingGame.h"
 
 @interface CardGameViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
-@property (strong, nonatomic) Deck *deck;
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
+@property (strong, nonatomic) CardMatchingGame *game;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
 @end
 
 @implementation CardGameViewController
 
-- (Deck *)deck {
-    // lazy instantiation of the deck
-    if (!_deck) _deck = [[PlayingCardDeck alloc] init];
+- (CardMatchingGame *)game {
+    // lazy instantiation
+    if (!_game) _game = [[CardMatchingGame alloc] initWithCardCount:[self.cardButtons count]
+                                                          usingDeck:[[PlayingCardDeck alloc] init]];
     
-    return _deck;
+    return _game;
 }
 
 - (void)setCardButtons:(NSArray *)cardButtons {
     _cardButtons = cardButtons;
-    
-    // iterate through card buttons and set them randomly
-    for (UIButton *cardButton in self. cardButtons) {
-        Card *card = [self.deck drawRandomCard];
+    [self updateUI];
+}
+
+- (void)updateUI {
+    for (UIButton *cardButton in self.cardButtons) {
+        Card *card = [self.game cardAtIndex:[self.cardButtons indexOfObject:cardButton]];
         
+        // set the title of the card
         [cardButton setTitle:card.contents forState:UIControlStateSelected];
+        [cardButton setTitle:card.contents forState:UIControlStateSelected|UIControlStateDisabled];
+
+        cardButton.selected = card.isFaceUp;
+        cardButton.enabled = !card.isUnplayable;
+        cardButton.alpha = (card.isUnplayable) ? 0.3 : 1;
     }
+    
+    self.scoreLabel.text = [NSString stringWithFormat:@"Score: %d", self.game.score];
 }
 
 - (void)setFlipCount:(int)flipCount {
@@ -42,8 +55,9 @@
 }
 
 - (IBAction)flipCard:(UIButton *)sender {
-    sender.selected = !sender.isSelected;
+    [self.game flipCardAtIndex:[self.cardButtons indexOfObject:sender]];
     self.flipCount++;
+    [self updateUI];
 }
 
 @end
--- Matchismo/Model/CardMatchingGame.m
+++ Matchismo/Model/CardMatchingGame.m
@@ -8,6 +8,85 @@
 
 #import "CardMatchingGame.h"
 
+@interface CardMatchingGame()
+@property (readwrite, nonatomic) int score; // private read/write
+@property (strong, nonatomic) NSMutableArray *cards; // of Card
+@end
+
 @implementation CardMatchingGame
+
+- (id)init {
+    // invalid, you should call the designated initializer
+    return nil;
+}
+
+- (NSMutableArray *)cards {
+    if (!_cards) _cards = [[NSMutableArray alloc] init];
+    return _cards;
+}
+
+- (id)initWithCardCount:(NSUInteger)count
+              usingDeck:(Deck *)deck
+{
+    self = [super init];
+    
+    if (self) {
+        for (int i = 0; i < count; i++) {
+            Card *card = [deck drawRandomCard];
+            
+            // got a card?
+            if (card) {
+                // yes, set it
+                self.cards[i] = card;
+            } else {
+                // no! count is larger than the number
+                // of cards in the deck
+                self = nil;
+                break;
+            }
+        }
+    }
+    
+    return self;
+}
+
+#define MATCH_BONUS 4
+#define MISMATCH_PENALTY 2
+#define FLIP_COST 1
+
+- (void)flipCardAtIndex:(NSUInteger)index {
+    Card *card = [self cardAtIndex:index];
+    
+    if (card && !card.isUnplayable) {
+        if (!card.isFaceUp) {
+            for (Card *otherCard in self.cards) {
+                if (otherCard.isFaceUp && !otherCard.isUnplayable) {
+                    int matchScore = [card match:@[ otherCard ]];
+                    
+                    if (matchScore) {
+                        card.unplayable = YES;
+                        otherCard.unplayable = YES;
+                        self.score += matchScore * MATCH_BONUS;
+                    } else {
+                        otherCard.faceUp = NO;
+                        self.score -= MISMATCH_PENALTY;
+                    }
+                    
+                    break;
+                }
+            }
+            
+            // discourage constant flipping
+            self.score -= FLIP_COST;
+        }
+
+        // flip the card
+        card.faceUp = !card.isFaceUp;
+    }
+}
+
+- (Card *)cardAtIndex:(NSUInteger)index {
+    return (index < [self.cards count]) ? self.cards[index] : nil;
+}
 
 @end
