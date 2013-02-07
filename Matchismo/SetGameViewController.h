2b1489a0b8f501b37b8b531f5f1bfab05339e490
Jeroen Wesbeek <work@osx.eu>
2013-02-07 12:15:07 +0100

    Assignment 2.1 - Add a tab bar controller to your application. One tab will be the game you built last week in Assignment 1. The other tab will be a new game, Set. Set is still a card game, so a good solution to this assignment will use object-oriented programming techniques to share a lot of code.

---
+++ Matchismo/SetGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetGameViewController.h
+//  Matchismo
+//
+//  Created by Jeroen Wesbeek on 2/7/13.
+//  Copyright (c) 2013 MyCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetGameViewController : UIViewController
+
+@end
---
+++ Matchismo/SetGameViewController.m
@@ -0,0 +1,17 @@
+//
+//  SetGameViewController.m
+//  Matchismo
+//
+//  Created by Jeroen Wesbeek on 2/7/13.
+//  Copyright (c) 2013 MyCompany. All rights reserved.
+//
+
+#import "SetGameViewController.h"
+
+@interface SetGameViewController ()
+@end
+
+@implementation SetGameViewController
+
+
+@end
